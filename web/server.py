72f7ab29cbda7545b285011806f65974e6aad478
Barak Korren <bkorren@redhat.com>
2021-03-05 14:34:05 +0200

    Use $PORT for server
    So it works in Heroku

--- web/server.py
+++ web/server.py
@@ -1,3 +1,4 @@
+from os import environ
 from sanic import Sanic
 from sanic.response import text
 
@@ -6,3 +7,6 @@ app = Sanic("My Hello, world app")
 @app.get("/")
 async def hello_world(request):
     return text("Hello, world. From Sanic")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(environ.get("PORT") or "8000"))
