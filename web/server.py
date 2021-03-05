b916264cf50c6e4262705ff3f5b91d0cef2cc386
Barak Korren <bkorren@redhat.com>
2021-03-05 12:08:20 +0200

    Adding a basic Sanic application
    Starting to structure things a bit for later.

---
+++ web/server.py
@@ -0,0 +1,8 @@
+from sanic import Sanic
+from sanic.response import text
+
+app = Sanic("My Hello, world app")
+
+@app.get("/")
+async def hello_world(request):
+    return text("Hello, world. From Sanic")
